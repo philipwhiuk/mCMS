929e64778c3b699de5949b0cd31c31e28fb57bf4
agentscorpion <agentscorpion@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2007-12-24 12:32:49 +0000

    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@14 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- api/html.php
+++ api/html.php
@@ -9,8 +9,21 @@
 class Output_HTML extends Output {
 
   function Run(){
-    // Load Theme etc
+    // Load Theme
+    $sql = "SELECT * FROM theme WHERE id = %u";
+    $result = Fusion::$_->storage->query($sql, Fusion::$_->user->theme);
+    if($result && $row = $result->fetch_assoc()){
+      if($row['type'] !== ''){
+        $c = 'Theme_' . $row['type'];
+      } else {
+        $c = 'Theme';
+      }
+      $this->theme = new $c($row);
+    }
+  }
   
+  function Output($template){
+    
   }
 
 }
--- api/page.php
+++ api/page.php
@@ -8,43 +8,11 @@
 
 class Page {
 
-  function authorised(){
-    // Implement
-    return true;
-  }
-
-  function __construct($data){
-    foreach($data as $f => $v){
-      $this->$f = $v;
-    }
-  }
-  
-  function load_blocks(){
-    $this->blocks = array();
-    $sql = "SELECT * FROM page_block LEFT JOIN block ON page_block.block_id = block.id WHERE page_id = %u";
-    $result = Fusion::$_->storage->query($sql, $alias);
-    if($result){
-      while($row = $result->fetch_assoc()){
-        $c = 'Block_' . $row['type'];
-        $obj = new $c($row);
-        if($obj->authorised()){
-          $this->blocks[$row['id']] = $obj;
-        }
-      }
-    }
-  }
-  
-  function run(){
-    $this->load_blocks();
-    foreach($this->blocks as $block){
-      $block->run();
-    }
-    return true;
-  }
-
   static function Load(){
     $aliasi = array();
+    $g = '';
     if(isset($_GET['page'])){
+      $g = $_GET['page'];
       $s = explode('/', $_GET['page']);
       $t = '';
       foreach($s as $p){
@@ -64,13 +32,89 @@ class Page {
           $c = 'Page';
         }
         Log::Message("{$row['name']} ({$row['type']}) page loaded.");
+        
+        if($g == ''){
+          $mode = '';
+        } else {
+          $mode = trim(substr($g, strlen($row['alias'])), '/');
+        }
+        
+        
         $obj = new $c($row);
-        if($obj->authorised()){
+        if($obj->authorised($mode)){
           return $obj;
         }
       }
     }
     Install::Page();
+  }
+
+  function __construct($data){
+    foreach($data as $f => $v){
+      $this->$f = $v;
+    }    
+  }
+  
+  function authorised($mode){
+    $ms = explode('/',$mode,2);
+    
+    // Implement authorisation here.    
+    $this->modes = array('view' => array());
+    
+    // Mode selector
+    if(isset($this->modes[$ms[0]])){
+      $this->mode = $mode;
+      $this->remainder = $ms[1];
+    } elseif(isset($this->modes['view'])){
+      $this->mode = 'view';
+      $this->remainder = $mode;
+    } else {
+      return false;
+    }
+    return true;
+  }
+  
+  // Loader functions
+  
+  function load_blocks(){
+    $this->blocks = array();
+    $sql = "SELECT * FROM page_block LEFT JOIN block ON page_block.block_id = block.id WHERE page_id = %u";
+    $result = Fusion::$_->storage->query($sql, $alias);
+    if($result){
+      while($row = $result->fetch_assoc()){
+        $c = 'Block_' . $row['type'];
+        $obj = new $c($row);
+        if($obj->authorised()){
+          $this->blocks[$row['id']] = $obj;
+        }
+      }
+    }
+  }
+  
+  // Main operations
+  
+  function run(){
+    $m = 'run_' . $this->mode;
+    return $this->$m($this->remainder);
+  }
+  
+  function output(){
+    $m = 'output_' . $this->mode;
+    return $this->$m();
+  }
+  
+  // View operations
+  
+  function run_view(){
+    $this->load_blocks();
+    foreach($this->blocks as $block){
+      $block->run();
+    }
+    return true;
+  }
+  
+  function output_view(){
+    // Create template and add blocks
   }
 
 }
