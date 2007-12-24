d715c94bcacc5f99e80eee14f2413d18b6294c9d
agentscorpion <agentscorpion@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2007-12-24 15:13:42 +0000

    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@15 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- api/html.php
+++ api/html.php
@@ -7,23 +7,9 @@
 **/
 
 class Output_HTML extends Output {
-
-  function Run(){
-    // Load Theme
-    $sql = "SELECT * FROM theme WHERE id = %u";
-    $result = Fusion::$_->storage->query($sql, Fusion::$_->user->theme);
-    if($result && $row = $result->fetch_assoc()){
-      if($row['type'] !== ''){
-        $c = 'Theme_' . $row['type'];
-      } else {
-        $c = 'Theme';
-      }
-      $this->theme = new $c($row);
-    }
-  }
   
   function Output($template){
-    
+    $this->Template('page');
   }
 
 }
--- api/output.php
+++ api/output.php
@@ -8,18 +8,52 @@
 
 class Output {
 
+  function __construct($name){
+    $this->name = $name;
+  }
+
+  function Run(){
+    // Load Theme
+    $sql = "SELECT * FROM theme WHERE id = %u";
+    $result = Fusion::$_->storage->query($sql, Fusion::$_->user->theme);
+    if($result && $row = $result->fetch_assoc()){
+      if($row['type'] !== ''){
+        $c = 'Theme_' . $row['type'];
+      } else {
+        $c = 'Theme';
+      }
+      $this->theme = new $c($row);
+    }
+  }
+
+  function Template($name){
+    $s = 'themes/' . $this->theme->folder . '/' . $this->name . '/' . $name . '.php';
+    $c = 'Template_' . $this->theme->folder . '_' . $this->name . '_' . str_replace('/','_',$name);
+    if(file_exists($s)){
+      require_once($s);
+      if(class_exists($c)){
+        return new $c;
+      }
+    }
+    return new Template;
+  }
+
   static function Load(){
     if(isset($_GET['output']) && class_exists('Output_' . $_GET['output'])){
       $c = 'Output_' . $_GET['output'];
-      return new $c();
+      return new $c($_GET['output']);
     }
     
     if(isset(Fusion::$_->config['output']) && class_exists('Output_' . Fusion::$_->config['output'])){
       $c = 'Output_' . Fusion::$_->config['output'];
-      return new $c();
+      return new $c(Fusion::$_->config['output']);
     }
     
     Install::Output();
   }
+
+}
+
+class Template {
 
 }
