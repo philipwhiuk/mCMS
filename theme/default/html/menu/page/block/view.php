577d0b17ea25a0bda7a579d14bc431dded43b697
agentscorpion <agentscorpion@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-02-13 17:30:57 +0000

    Updated Menu View to latest version.
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@68 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- theme/default/html/menu/page/block/view.php
+++ theme/default/html/menu/page/block/view.php
@@ -1,35 +1,68 @@
 <?php
 
 class Template_Theme_Default_HTML_Menu_Page_Block_View extends Template {
-	
-	public function display_section($links, $level){
-		if($level > 4){ return; }
-?> 
-		<ul>
-			<?php foreach($links as $link){ ?> 
-				<li class="<?php if($link['trail']){ ?>menu-trail<?php } ?> <?php if($link['current']){ ?>menu-current<?php } ?>">
-					<a href="<?php echo $link['url']; ?>" class="<?php if($link['trail']){ ?>menu-trail<?php } ?> <?php if($link['current']){ ?>menu-current<?php } ?>">
-						<?php echo $link['name']; ?> 
-					</a>
-					<?php 
-						if(count($link['children']) > 0){ 
-							$this->display_section($link['children'], $level + 1);
-						}		
-					?> 
-				</li>
-			<?php } ?> 
-		</ul>
+
+	public function display(){
+		if($this->level == 0){
+			$this->display_top();
+		} else {
+			$this->display_child();
+		}
+	}
+
+	public function display_child(){
+?>
+<div class="menu-child">
+	<ul>
+<?php 
+		foreach($this->items as $item){
+			$class = array();
+			if($item['trail']){
+				$class[] = 'menu-trail';
+			}
+			if($item['current']){
+				$class[] = 'menu-current';
+			}
+	?> 
+		<li <?php if(count($class) > 0){ ?> class="<?php echo join(' ',$class);?>" <?php } ?>>
+			<a href="<?php echo $item['url']; ?>"<?php if(count($class) > 0){ ?> class="<?php echo join(' ',$class);?>" <?php } ?>>
+				<?php echo $item['name']; ?> 
+			</a>
+<?php if($item['child']){ $item['child']->display(); } ?> 
+		</li>
+<?php
+		}
+	?>
+	</ul>
+</div>
 <?php
 	}
-	
-	public function display(){
+
+	public function display_top(){
 ?>		
 <div class="menu page-block-menu menu-view page-block-menu-view">
-	<?php 
-		if(isset($this->items[0]) && count($this->items[0]['children']) > 0){
-			$this->display_section($this->items[0]['children'], 1);
+	<ul>
+<?php 
+		foreach($items as $item){
+			$class = array();
+			if($item['trail']){
+				$class[] = 'menu-trail';
+			}
+			if($item['current']){
+				$class[] = 'menu-current';
+			}
+	?>
+		<li<?php if(count($class) > 0){ ?> class="<?php echo join(' ',$class);?>" <?php } ?>>
+			<a href="<?php echo $item['url']; ?>"<?php if(count($class) > 0){ ?> class="<?php echo join(' ',$class);?>"<?php } ?>>
+				<?php echo $item['name']; ?> 
+			</a>
+<?php if($item['child']){ $item['child']->display(); } ?> 
+		</li>
+<?php
 		}
 	?> 
+		</li>
+	</ul>
 </div>
 <?php	
 	}	
