88ea5ebf5cc19db7abbc79accda41542c4aad297
philip.whitehouse <philip.whitehouse@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-06-07 20:16:07 +0000

    Added Authentication Retrieval Exception (exception called by authentication.php but exception previously not found.)
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@93 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- authentication/exception.php
+++ authentication/exception.php
@@ -2,3 +2,4 @@
 
 class Authentication_Exception extends CMS_Exception {}
 class Authentication_User_Exception extends Authentication_Exception {}
+class Authentication_Retrieval_Exception extends Authentication_Exception {}
