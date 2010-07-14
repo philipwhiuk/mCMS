ab6f25ccd415edb2914a34f7bf1842d809b416d9
philip.whitehouse <philip.whitehouse@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-14 15:46:48 +0000

    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@106 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- film_feature/page_block_main/category_next.php
+++ film_feature/page_block_main/category_next.php
@@ -1,9 +1,10 @@
 <?php
 class Film_Feature_Page_Block_Main_Category_Next extends Film_Feature_Page_Block_Main {
-	function __construct($parent, $feature) {
+	function __construct($parent, $feature,$cat) {
 		$this->feature = $feature;
 		$this->feature->get_showings();
-		Module::Get('film_feature')->file('film_feature_impl//page_block_main/category_next');
+		$this->category = $cat;
+		Module::Get('film_feature')->file('film_feature_impl/page_block_main/category_next');
 		$this->films = $this->feature->get_films();
 		$class = $this->feature->get_module()->load_section('Film_Feature_Impl_Page_Block_Main_Category_Next');
 		foreach($this->films as $film) {
@@ -11,12 +12,36 @@ class Film_Feature_Page_Block_Main_Category_Next extends Film_Feature_Page_Block
 		}
 	}
 	function display() {
+	try {
 		$this->system = System::Get_Instance();
-		$this->module = Module::Get('event');
-		$template = $this->system->output()->start(array('film_feature', 'event','page','block','next'));
+		$this->module = Module::Get('film_feature');
+		$template = $this->system->output()->start(array('film_feature','page','block','category','next'));
+		$template->category = $this->category;
+		$featureID = $this->feature->get_id();
+		$res = Resource::Get_By_Argument($this->module, $featureID);
+		$resurl = $res->url();
+		$template->url = $this->system->url($resurl);
+		$cdate = 0;
+		foreach($this->feature->get_showings() as $showing) {
+			$newdate = date('dmY', $showing->get_datetime());
+			$showingsCount = 0;
+			if($newdate != $cdate) {
+				$cdate = $newdate;
+				$showingsCount++;
+				$template->showings[$showingsCount] = array();
+				$template->showings[$showingsCount][] = $showing->get_datetime();
+			}
+			else { 
+				$template->showings[$showingsCount][] = $showing->get_datetime();
+			}
+		}
 		foreach($this->implViews as $implView) {
 			$template->films[] = $implView->display();
 		}
 		return $template;
+	}
+	catch(Exception $e) {
+		var_dump($e);
+	}
 	}
 }
