e91c258294451941bbffcaa060da54797ec3e336
rjw201@cam.ac.uk <rjw201@cam.ac.uk@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-16 01:57:41 +0000

    TinyMCE Exception error
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@133 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- tinymce/exception.php
+++ tinymce/exception.php
@@ -6,4 +6,4 @@ class TinyMCE_Page_Main_Exception extends TinyMCE_Exception {}
 
 class TinyMCE_Field_Invalid_Exception extends TinyMCE_Exception {}
 
-class TinyMCE_Page_Main_Files_Type_Exception extends TinyMCE_Page_Main_Files_Exception {}
+class TinyMCE_Page_Main_Files_Type_Exception extends TinyMCE_Exception {}
