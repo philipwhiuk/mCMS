77e69f2a76d277e42ebddfe48bfcc0838df3f93e
rjw201@cam.ac.uk <rjw201@cam.ac.uk@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-16 01:55:58 +0000

    TinyMCE not loading exceptions.
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@132 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- tinymce/module.php
+++ tinymce/module.php
@@ -5,6 +5,8 @@ class TinyMCE_Module extends Module {
 	public function load(){
 		Module::Get('form', 'inline', 'file', 'image');
 		
+		$this->file('exception');
+
 		Form_Field::Register('richtext', 'TinyMCE_Field', 'field', $this);
 		
 	}
