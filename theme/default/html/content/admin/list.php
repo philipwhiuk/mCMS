ea53fdecf140d9e0d75090c85b7c8b1bfa0575ed
rjw201@cam.ac.uk <rjw201@cam.ac.uk@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-17 14:30:33 +0000

    Made Default Theme work properly with content admin
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@138 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- theme/default/html/content/admin/list.php
+++ theme/default/html/content/admin/list.php
@@ -4,21 +4,24 @@ class Template_Theme_Default_HTML_Content_Admin_List extends Template {
 	public function display(){
 ?>		
 <div class="content-admin-list">
-	<table> 
+	<h1><?php echo $this->title; ?></h1>
+	<table cellspacing=0 cellpadding=0> 
 <?php 
+		$a = 0;
 		foreach($this->content as $content){
 ?>
-		<tr>
+		<tr <?php if($a == 0){ ?> class="even" <?php } else { ?> class="odd" <?php } ?>>
 			<td>
 				<?php echo $content['title']; ?> 
 			</td>
-			<td>
+			<td class="link">
 				<a href="<?php echo $content['edit']; ?>">
 					<?php echo $this->edit; ?> 
 				</a>
 			</td>
 		</tr> 
 <?php
+			$a = ($a + 1) % 2;
 		}
 ?> 
 	</table>
