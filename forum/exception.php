382686e726c0feeaabac60d4ad4655ba39c0cc94
rjw201@cam.ac.uk <rjw201@cam.ac.uk@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-21 23:07:24 +0000

    Or this
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@169 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- forum/exception.php
+++ forum/exception.php
@@ -1,9 +1,8 @@
 <?php
-class Forum_Module_Exception extends CMS_Exception {}
-class Forum_Exception extends Forum_Module_Exception {}
+class Forum_Exception extends CMS_Exception {}
 class Forum_Not_Found_Exception extends Forum_Exception {}
 class Forum_Unavailable_Exception extends Forum_Exception {}
-class Forum_Page_Main_Exception extends Forum_Module_Exception {}
-class Forum_Topic_Exception extends Forum_Module_Exception {}
+class Forum_Page_Main_Exception extends Forum_Exception {}
+class Forum_Topic_Exception extends Forum_Exception {}
 class Forum_Topic_Not_Found_Exception extends Forum_Exception {}
 class Forum_Topic_Unavailable_Exception extends Forum_Exception {}
--- news/exception.php
+++ news/exception.php
@@ -2,10 +2,10 @@
 
 class News_Exception extends CMS_Exception {}
 
-class News_Category_Exception extends News_Module_Exception {}
+class News_Category_Exception extends News_Exception {}
 
 class News_Category_Not_Found_Exception extends News_Category_Exception {}
 
-class News_Page_Main_Exception extends News_Module_Exception {}
+class News_Page_Main_Exception extends News_Exception {}
 
 class News_Page_Main_Unavailable_Exception extends News_Page_Main_Exception {}
