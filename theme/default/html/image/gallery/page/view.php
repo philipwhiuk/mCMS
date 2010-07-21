5c60596ee3d1c02ec9f186887e6e16a716d0bfc5
rjw201@cam.ac.uk <rjw201@cam.ac.uk@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-21 15:03:33 +0000

    Corrected bug relating to updated Gallery code.
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@163 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- theme/default/html/image/gallery/page/view.php
+++ theme/default/html/image/gallery/page/view.php
@@ -2,56 +2,38 @@
 
 class Template_Theme_Default_HTML_Image_Gallery_Page_View extends Template {
 
-	public function __construct($parent){
-		parent::__construct($parent);
-		$this->html->head('jquery', '<script language="javascript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js
-"></script>');
-		$this->html->head('image', '<script language="javascript" type="text/javascript" src="' . $this->theme->url('js/image.js') . '"></script><script language="javascript" type="text/javascript">image_init("' . $this->theme->url('images/loadingAnimation.gif') . '","' . $this->theme->url('images/tbclose.png') . '");</script>');
-	}
-
-// Replace with image code.
-	public function get_file($target){
-		ksort($this->files); 
-		$current = null; $cfile = null;
-		foreach($this->files as $match => $file){
-			if($match == $target){
-				return $file;
-			}
-			if(!isset($current) && $match > $target){
-				return $file;
-			} elseif(!isset($current)){
-				$current = $match; $cfile = $file;
-			} elseif($match < $target){
-				$current = $match; $cfile = $file;
-			} else { //if($current < $target, $match > $target){
-				return $file;
-			}
-		}
-		return $cfile;
-	}
-
 	public function display($others = array()){
 		 $p = count($others);
+		 $system = System::Get_Instance();
 
 ?>
 		<div class="gallery-items-images-selector">
-			<ul> 
+			<div class="a">
 			<?php 
 				$i = 0;
 				foreach($others as $other){
 					$i ++;
 					if($i == 1){ ?>
-			
+				<div class="b"> 
 				<?php 	} ?>
-					<li><a href="<?php echo $other->furl; ?>">
-						<img src="<?php $file = $other->get_file(110); echo $file['url']; ?>">							</a></li>
+					<a class="gallery-items-images-image" href="<?php echo $other->furl; ?>"><div class="c"><div class="d">
+						<div class="e" style="background-image: url('<?php 
+							echo $other->image->raw_url('max/300/150');
+						 ?>');"></div>
+					</div></div></a>
 				<?php 	if($i == 5){ ?>
+				</div>
 				<?php	$i = 0;
 					} 
 				}
-				if($i != 0){ ?></div><?php } 
+				if($i != 0){
+					for(; $i < 5; $i ++){
+						?><a></a><?php
+					}
+					?>
+				</div><?php } 
 			?>
-			</ul>
+			</div>
 		</div>
 <?php
 	}
