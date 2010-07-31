6cd44573be757e73969a1b30127455a6154c7bba
philip.whitehouse <philip.whitehouse@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-07-31 20:39:16 +0000

    Fixed if() statement and removed var_dump
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@186 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- theme/default/html/content/page/block/view.php
+++ theme/default/html/content/page/block/view.php
@@ -4,9 +4,8 @@ class Template_Theme_Default_HTML_Content_Page_Block_View extends Template {
 	public function display(){
 ?>		
 <div class="content page-block-content">
-	<?php var_dump($this);
-		
-		if(isset($this->title && $this->title != "") { ?><h1><?php echo $this->title; ?></h1><?php } ?>
+	<?php		
+		if(isset($this->title) && $this->title != "") { ?><h1><?php echo $this->title; ?></h1><?php } ?>
 	<?php echo $this->body; ?> 
 </div>
 <?php	
