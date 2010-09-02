89aef8f415ca0262634ebcefbb9ccf48b47c8d5c
philip.whitehouse <philip.whitehouse@c98e835a-613f-0410-a8fd-ab24993f6fe8>
2010-09-02 01:17:58 +0000

    execute_add added to Film_Feature_Admin
    git-svn-id: https://fusion-web.googlecode.com/svn/trunk@201 c98e835a-613f-0410-a8fd-ab24993f6fe8

--- film_feature/admin.php
+++ film_feature/admin.php
@@ -28,6 +28,31 @@ class Film_Feature_Admin extends Admin {
 			) ? -1 : 1;
 		}
 	}
+	public function execute_add(){
+		$this->mode = 'add';
+		$language = Language::Retrieve();
+		$this->form = new Form(array('film','admin'), $this->url('add'));
+		
+		$title = Form_Field::Create('title', array('textbox'));
+		$title->set_label($language->get($this->module, array('admin','add','title')));
+		$title->set_value($this->film_feature->get_content()->get_title());
+		
+		$description = Form_Field::Create('description', array('richtext','textarea'));
+		$description->set_label($language->get($this->module, array('admin','add','description')));
+		$description->set_value($this->film_feature->get_content()->get_body());
+		
+		$category = Form_Field::Create('category', array('select'));
+		$category->set_label($language->get($this->module, array('admin','edit','category')));
+		$cats = Film_Feature_Category::Get_All();
+		foreach ($cats as $cat) {
+			$category->set_option($cat->get_id(),$cat->description()->get_title(),true);
+		}
+		
+		$filmForm = new Form_Field(array('film', 'admin'), $this->url('add'));
+		//do film
+		$showingForm = new Form(array('showing', 'admin'), $this->url('add'));
+		$submit = Form_Field::Create('submit', array('submit'));
+	}
 	public function execute_list(){
 		$this->mode = 'list';  
 		$arg = $this->parent->resource()->get_argument();
