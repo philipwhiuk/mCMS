4c1459bd12ac9f1b0f613efcdcce1f7fa2204514
Philip Whitehouse <philip@whiuk.com>
2012-08-26 12:43:04 +0100

    Initial work on Film_Festival_Admin

--- film/film_festival/admin/menu.php
+++ film/film_festival/admin/menu.php
@@ -1,4 +1,24 @@
 <?php
 class Film_Festival_Admin_Menu extends Film_Festival_Admin {
+	public function __construct($a,$b){
+		parent::__construct($a,$b);
+		$this->menu_title = Language::Retrieve()->get($this->module, array('admin','menu','title'));
+		$this->menu_items = array(
+			array('title' => Language::Retrieve()->get($this->module, array('admin','menu','Add')),
+				  'url' => $this->url().'add/'),
+			array('title' => Language::Retrieve()->get($this->module, array('admin','menu','Manage')),
+				  'url' => $this->url().'list/'),
+			array('title' => Language::Retrieve()->get($this->module, array('admin','menu','Permissions')),
+				  'url' => $this->url().'permissions/'),			  
+		);
+	}
 
+	public function display($selected=false){
+		$template = MCMS::Get_Instance()->output()->start(array('film_festival','admin','menu'));
+		$template->title = $this->menu_title;
+		$template->items = $this->menu_items;
+		$template->url = $this->url;
+		$template->selected = $selected;
+		return $template;
+	}
 }
