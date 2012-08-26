c1b919377f7caa70fe654262cfa7ef62962f0880
Philip Whitehouse <philip@whiuk.com>
2012-08-26 11:05:03 +0100

    Initial work on Forum_Admin

--- forum/admin.php
+++ forum/admin.php
@@ -32,62 +32,14 @@ class Forum_Admin extends Admin {
 	public function __construct($a,$b){
 		parent::__construct($a,$b);
 		$this->url = $this->url();
-		Permission::Check(array('film_festival'), array('view','edit','add','delete','list','admin'),'admin');
+		Permission::Check(array('forum'), array('view','edit','add','delete','list','admin'),'admin');
 
-	}
-	public function display() {
-		if($this->mode == 'list'){
-			return $this->display_list();
-		} elseif($this->mode == 'categories'){
-			return $this->display_categories();
-		} elseif($this->mode == 'edit'){
-			return $this->display_edit();
-		} 
 	}
 	public function display_edit(){
 		$template = MCMS::Get_Instance()->output()->start(array('news','admin','edit'));
 		$template->title = $this->forum->content()->get_title();
 		$template->form = $this->form->display();
 		return $template;
-	}
-	public function display_list(){
-		$template = MCMS::Get_Instance()->output()->start(array('forum','admin','list'));
-		$template->forum = array();
-		$template->edit = $this->edit;
-		$template->title = $this->title;
-		$template->pages = $this->pages;
-		$template->page_count = $this->page_count;
-		$template->page = $this->page;
-		foreach($this->forum as $forum){
-			$template->forum[] = array(
-				'title' => $forum->content()->get_title(),
-				'edit' => $this->url('edit/' . $forum->id())
-			);
-		}
-		return $template;
-	}
-
-	public function execute($parent){
-		$this->parent = $parent;
-		$arg = $this->parent->resource()->get_argument();
-		try {
-			if($arg == 'list'){
-				$this->parent->resource()->consume_argument();
-				$this->execute_list();
-				return;
-			} elseif($arg == 'posts') {
-				$this->parent->resource()->consume_argument();
-				$this->execute_categories();
-				return;
-			} elseif($arg == 'edit'){
-				$this->parent->resource()->consume_argument();
-				$this->execute_edit();
-				return;
-			}
-		} catch(Exception $e){
-			var_dump($e);
-		}
-		$this->execute_list();
 	}
 	public function execute_edit(){
 		$this->mode = 'edit';
@@ -167,32 +119,5 @@ class Forum_Admin extends Admin {
 
 		$this->form->fields($title,$description,$language_field,$parent,$depth,$has_topics,$submit);
 	}
-	public function execute_list() {
-		$this->mode = 'list';  
-		$arg = $this->parent->resource()->get_argument();
-		if(is_numeric($arg) && ((int) $arg) > 0){
-			$page = $arg;
-		}
-		else {
-			$page = 1;
-		}
-		$forums = Forum::Get_All();
-		if(!(($page-1)*20 <= count($forums))) {
-			$page = (int) (count($forums)/20)-1;
-		}
-		$this->forum = array();
-		for($i = ($page-1)*20; $i < $page*20 && $i < count($forums); $i++) {
-			if($forums[$i] instanceof Forum) { $this->forum[] = $forums[$i]; }
-		}
-		$this->page = $page;
-		$count = Forum::Count_All();
-		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
-		$this->pages = array();
-		for($pg = 1; $pg <= $this->page_count; $pg ++){
-			$this->pages[$pg] = $this->url('list/' . $pg);
-		}	
-		$language = Language::Retrieve();
-		$this->edit = $language->get($this->module, array('admin','list','edit'));
-		$this->title = $language->get($this->module, array('admin','list','title'));
-	}
+
 }
--- forum/admin/list.php
+++ forum/admin/list.php
@@ -1,4 +1,48 @@
 <?php
 class Forum_Admin_List extends Forum_Admin {
-
+	public function __construct($a,$b){
+		parent::__construct($a,$b);
+		$this->mode = 'list';  
+		$arg = $this->parent->resource()->get_argument();
+		if(is_numeric($arg) && ((int) $arg) > 0){
+			$page = $arg;
+		}
+		else {
+			$page = 1;
+		}
+		$forums = Forum::Get_All();
+		if(!(($page-1)*20 <= count($forums))) {
+			$page = (int) (count($forums)/20)-1;
+		}
+		$this->forum = array();
+		for($i = ($page-1)*20; $i < $page*20 && $i < count($forums); $i++) {
+			if($forums[$i] instanceof Forum) { $this->forum[] = $forums[$i]; }
+		}
+		$this->page = $page;
+		$count = Forum::Count_All();
+		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
+		$this->pages = array();
+		for($pg = 1; $pg <= $this->page_count; $pg ++){
+			$this->pages[$pg] = $this->url('list/' . $pg);
+		}	
+		$language = Language::Retrieve();
+		$this->edit = $language->get($this->module, array('admin','list','edit'));
+		$this->title = $language->get($this->module, array('admin','list','title'));
+	}
+	public function display(){
+		$template = MCMS::Get_Instance()->output()->start(array('forum','admin','list'));
+		$template->forum = array();
+		$template->edit = $this->edit;
+		$template->title = $this->title;
+		$template->pages = $this->pages;
+		$template->page_count = $this->page_count;
+		$template->page = $this->page;
+		foreach($this->forum as $forum){
+			$template->forum[] = array(
+				'title' => $forum->content()->get_title(),
+				'edit' => $this->url('edit/' . $forum->id())
+			);
+		}
+		return $template;
+	}
 }
