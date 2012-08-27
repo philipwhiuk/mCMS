6507403d98ef4f3fd8e758b7e77481ebe6340260
Philip Whitehouse <philip@whiuk.com>
2012-08-27 17:17:39 +0100

    Built Actor_Admin_List

--- film/actor/admin.php
+++ film/actor/admin.php
@@ -55,30 +55,7 @@ abstract class Actor_Admin extends Admin {
 	}
 	public function execute_list(){
 		$this->mode = 'list';  
-		$arg = $this->parent->resource()->get_argument();
-		if(is_numeric($arg) && ((int) $arg) > 0){
-			$page = $arg;
-		}
-		else {
-			$page = 1;
-		}
-		$actors = Actor::Get_All();
-		usort($actors,array("Actor_Admin","actor_sort"));
-		if(!(($page)*20 < count($actors)) && $page > 1) {
-			$page = (int) round(count($actors)/20);
-		}
-		for($i = ($page-1)*20; ($i < $page*20 && $i < count($actors)); $i++) {
-			if($actors[$i] instanceof Actor) { $this->actor[] = $actors[$i]; }
-		}
-		$this->page = $page;
-		$count = Actor::Count_All();
-		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
-		$language = Language::Retrieve();
-		$this->edit = $language->get($this->module, array('admin','list','edit'));
-		$this->page_title = $language->get($this->module, array('admin','list','title'));
-		for($pg = 1; $pg <= $this->page_count; $pg ++){
-			$this->pages[$pg] = $this->url('list/' . $pg);
-		}		
+	
 	}
 
 	public function execute_edit(){
@@ -122,22 +99,6 @@ abstract class Actor_Admin extends Admin {
 
 
 
-	public function display_list(){
-		$template = MCMS::Get_Instance()->output()->start(array('actor','admin','list'));
-		$template->actor = array();
-		$template->edit = $this->edit;
-		$template->title = $this->page_title;
-		$template->pages = $this->pages;
-		$template->page_count = $this->page_count;
-		$template->page = $this->page;
-		foreach($this->actors as $actor){
-			$template->actors[] = array(
-				'title' => $actor->get_description()->get_title(),
-				'edit' => $this->url('edit/' . $actor->get_id())
-			);
-		}
-		return $template;
-	}
 
 	public function display_edit(){
 		$template = MCMS::Get_Instance()->output()->start(array('actor','admin','edit'));
