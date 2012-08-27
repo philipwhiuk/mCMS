077b6875b266641fedc84c9705f35529e88ba168
Philip Whitehouse <philip@whiuk.com>
2012-08-27 20:04:20 +0100

    Fixed Default Film_Festival_Page_List
    Fixed classname
    Added title
    Added output list

--- theme/default/html/film_festival/page/list.php
+++ theme/default/html/film_festival/page/list.php
@@ -1,8 +1,13 @@
 <?php
-class Template_theme_flix_html_film_festival_page_list extends Template {
+class Template_Theme_Default_HTML_Film_Festival_Page_List extends Template {
 	function display() {
 		?><div class="page-view">
-		&nbsp;
+		<h1><?php echo $this->title; ?></h1>
+		<ul>
+			<?php foreach($this->items as $film_festival){ ?>
+			<li><a href="<?php echo $film_festival['url']; ?>"><?php echo $film_festival['name']; ?></a></li>
+			<?php } ?>
+		</ul> 
 		</div><?php
 	}
 }
