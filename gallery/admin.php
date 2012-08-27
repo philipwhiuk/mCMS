1a470b02016ce8f4d31dfca7c496c4ae22e7214d
Philip Whitehouse <philip@whiuk.com>
2012-08-27 17:17:50 +0100

    Built Gallery_Admin_List

--- gallery/admin.php
+++ gallery/admin.php
@@ -42,29 +42,6 @@ class Gallery_Admin extends Admin {
 
 	}
 
-	public function execute_list(){
-		$this->mode = 'list';  
-		$arg = $this->parent->resource()->get_argument();
-		if(is_numeric($arg) && ((int) $arg) > 0){
-			$arg = (int) $arg;
-			$this->galleries = Gallery::Get_All(20, ($arg - 1) * 20);
-			$this->parent->resource()->consume_argument();
-			$this->page = $arg;
-		} else {
-			$this->page = 1;
-			$this->galleries = Gallery::Get_All(20);
-		}
-
-		$count = Gallery::Count_All();
-		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
-		$language = Language::Retrieve();
-		$this->edit = $language->get($this->module, array('admin','list','edit'));
-		$this->title = $language->get($this->module, array('admin','list','title'));
-		for($pg = 1; $pg <= $this->page_count; $pg ++){
-			$this->pages[$pg] = $this->url('list/' . $pg);
-		}
-	}
-
 	public function execute_edit(){
 		$this->mode = 'edit';
 		$arg = $this->parent->resource()->get_argument();
@@ -127,22 +104,7 @@ class Gallery_Admin extends Admin {
 		return $template;
 	}
 
-	public function display_list(){
-		$template = MCMS::Get_Instance()->output()->start(array('gallery','admin','list'));
-		$template->content = array();
-		$template->edit = $this->edit;
-		$template->title = $this->title;
-		$template->pages = $this->pages;
-		$template->page_count = $this->page_count;
-		$template->page = $this->page;
-		foreach($this->galleries as $gallery){
-			$template->galleries[] = array(
-				'title' => $gallery->content()->get_title(),
-				'edit' => $this->url('edit/' . $gallery->id())
-			);
-		}
-		return $template;
-	}
+
 
 	public function display_edit(){
 		$template = MCMS::Get_Instance()->output()->start(array('gallery','admin','edit'));
