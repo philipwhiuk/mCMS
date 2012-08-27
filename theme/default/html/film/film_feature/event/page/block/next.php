fb9cfd53c4ed17a899e17edaaceb8756556a328f
Philip Whitehouse <philip@whiuk.com>
2012-08-27 19:55:26 +0100

    Fixed template class names not matching theme name

--- theme/default/html/film/film_feature/event/page/block/next.php
+++ theme/default/html/film/film_feature/event/page/block/next.php
@@ -1,5 +1,5 @@
 <?php
-	class Template_Theme_Flix_HTML_Film_Film_Feature_Event_Page_Block_Next extends Template {
+	class Template_Theme_Default_HTML_Film_Film_Feature_Event_Page_Block_Next extends Template {
 		public $name;
 		function display() {
 			?>
