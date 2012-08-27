ceef986063741805eb3def8db4b63e3caeb6f4cc
Philip Whitehouse <philip@whiuk.com>
2012-08-27 16:47:58 +0100

    Removed functions now part of UA_List

--- core/user/admin.php
+++ core/user/admin.php
@@ -46,23 +46,7 @@ class User_Admin extends Admin {
 		$template->form = $this->form->display();
 		return $template;
 	}	
-	public function display_list(){
-		$template = MCMS::Get_Instance()->output()->start(array('user','admin','list'));
-		$template->content = array();
-		$template->edit = $this->edit;
-		$template->title = $this->title;
-		$template->pages = $this->pages;
-		$template->page_count = $this->page_count;
-		$template->page = $this->page;
-		foreach($this->user as $user){
-			$template->user[] = array(
-				'id' => $user->get_id(),
-				'username' => $user->get('local_username'),
-				'edit' => $this->url('edit/' . $user->get_id())
-			);
-		}
-		return $template;
-	}
+
 
 	public function execute($parent){
 		$this->parent = $parent;
@@ -81,28 +65,6 @@ class User_Admin extends Admin {
 
 		}
 		$this->execute_list();
-	}
-	public function execute_list(){
-		$this->mode = 'list';  
-		$arg = $this->parent->resource()->get_argument();
-		if(is_numeric($arg) && ((int) $arg) > 0){
-			$arg = (int) $arg;
-			$this->user = User::Get_All(20, ($arg - 1) * 20);
-			$this->parent->resource()->consume_argument();
-			$this->page = $arg;
-		} else {
-			$this->page = 1;
-			$this->user = User::Get_All(20);
-		}
-
-		$count = User::Count_All();
-		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
-		$language = Language::Retrieve();
-		$this->edit = $language->get($this->module, array('admin','list','edit'));
-		$this->title = $language->get($this->module, array('admin','list','title'));
-		for($pg = 1; $pg <= $this->page_count; $pg ++){
-			$this->pages[$pg] = $this->url('list/' . $pg);
-		}
 	}
 	public function execute_edit(){
 		$this->mode = 'edit';
