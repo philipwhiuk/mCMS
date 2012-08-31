c0986ee62b35a733fea122e4b54d12bd57bb31da
Philip Whitehouse <philip@whiuk.com>
2012-08-31 23:45:53 +0100

    Updating Menu_Admin

--- menu/admin.php
+++ menu/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-class Menu_Admin extends Admin {
+abstract class Menu_Admin extends Admin {
 
 	protected $parent;
 	protected $mode;
@@ -39,30 +39,6 @@ class Menu_Admin extends Admin {
 		Permission::Check(array('menu'), array('view','edit','add','delete','list','admin'),'admin');
 
 	}
-
-	public function execute_list(){
-		$this->mode = 'list';  
-		$arg = $this->parent->resource()->get_argument();
-		if(is_numeric($arg) && ((int) $arg) > 0){
-			$arg = (int) $arg;
-			$this->menus = Menu::Get_All(20, ($arg - 1) * 20);
-			$this->parent->resource()->consume_argument();
-			$this->page = $arg;
-		} else {
-			$this->page = 1;
-			$this->menus = Menu::Get_All(20);
-		}
-
-		$count = Menu::Count_All();
-		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
-		$language = Language::Retrieve();
-		$this->edit = $language->get($this->module, array('admin','list','edit'));
-		$this->title = $language->get($this->module, array('admin','list','title'));
-		for($pg = 1; $pg <= $this->page_count; $pg ++){
-			$this->pages[$pg] = $this->url('list/' . $pg);
-		}
-	}
-
 	public function execute_edit(){
 		$this->mode = 'edit';
 		$arg = $this->parent->resource()->get_argument();
@@ -97,58 +73,10 @@ class Menu_Admin extends Admin {
 
 	}
 
-	public function execute($parent){
-		$this->parent = $parent;
-		$arg = $this->parent->resource()->get_argument();
-		try {
-			if($arg == 'list'){
-				$this->parent->resource()->consume_argument();
-				$this->execute_list();
-				return;
-			} elseif($arg == 'edit'){
-				$this->parent->resource()->consume_argument();
-				$this->execute_edit();
-				return;
-			}
-		} catch(Exception $e){
-
-		}
-		$this->execute_list();
-	}
-
-
-
-	public function display_list(){
-		$template = MCMS::Get_Instance()->output()->start(array('menu','admin','list'));
-		$template->content = array();
-		$template->edit = $this->edit;
-		$template->title = $this->title;
-		$template->pages = $this->pages;
-		$template->page_count = $this->page_count;
-		$template->page = $this->page;
-		foreach($this->menus as $menu){
-			$template->menus[] = array(
-				'name' => $menu->name(),
-				'edit' => $this->url('edit/' . $menu->id())
-			);
-		}
-		return $template;
-	}
-
 	public function display_edit(){
 		$template = MCMS::Get_Instance()->output()->start(array('menu','admin','edit'));
 		$template->title = $this->content->get_title();
 		$template->form = $this->form->display();
 		return $template;
 	}
-
-
-	public function display(){
-		if($this->mode == 'list'){
-			return $this->display_list();
-		} elseif($this->mode == 'edit'){
-			return $this->display_edit();
-		} 
-	}
-
 }
--- menu/admin/list.php
+++ menu/admin/list.php
@@ -1,4 +1,42 @@
 <?php
 class Menu_Admin_List extends Menu_Admin {
+	public function __construct($a,$b){
+		parent::__construct($a,$b);
+		$arg = $this->parent->resource()->get_argument();
+		if(is_numeric($arg) && ((int) $arg) > 0){
+			$arg = (int) $arg;
+			$this->menus = Menu::Get_All(20, ($arg - 1) * 20);
+			$this->parent->resource()->consume_argument();
+			$this->page = $arg;
+		} else {
+			$this->page = 1;
+			$this->menus = Menu::Get_All(20);
+		}
+
+		$count = Menu::Count_All();
+		$this->page_count = ((int) ($count / 20)) + ((($count % 20) == 0) ? 0 : 1);
+		$language = Language::Retrieve();
+		$this->edit = $language->get($this->module, array('admin','list','edit'));
+		$this->title = $language->get($this->module, array('admin','list','title'));
+		for($pg = 1; $pg <= $this->page_count; $pg ++){
+			$this->pages[$pg] = $this->url('list/' . $pg);
+		}
+	}
+	public function display(){
+		$template = MCMS::Get_Instance()->output()->start(array('menu','admin','list'));
+		$template->content = array();
+		$template->edit = $this->edit;
+		$template->title = $this->title;
+		$template->pages = $this->pages;
+		$template->page_count = $this->page_count;
+		$template->page = $this->page;
+		foreach($this->menus as $menu){
+			$template->menus[] = array(
+				'name' => $menu->name(),
+				'edit' => $this->url('edit/' . $menu->id())
+			);
+		}
+		return $template;
+	}
 
 }
