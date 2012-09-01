e96c3faddf893a5644c90779aa941de7bfd751c7
Philip Whitehouse <philip@whiuk.com>
2012-09-01 22:19:41 +0100

    Removing Simm

--- simm/mission.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Simm_Mission {
-	public static function Count_All(){
-		
-		$query = MCMS::Get_Instance()->Storage()->Count()->From('simm_mission');
-		return $query->execute();
-
-	}
-}
